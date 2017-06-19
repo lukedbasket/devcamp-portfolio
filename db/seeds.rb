e94c2c4c50eb7e3f6ce1a35ed2fd273bcdfdb2e8
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 15:56:29 +0000

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ puts "5 skills created"
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x150"
+    thumb_image: "http://placehold.it/350x200"
     )
 end
 
