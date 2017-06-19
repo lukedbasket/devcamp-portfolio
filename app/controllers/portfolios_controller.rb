437641473badb6d95e198aa9823250584decda15
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 18:04:50 +0000

    Created edit feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,16 @@
 class PortfoliosController < ApplicationController
+  # before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
   def index
     @portfolio_items = Portfolio.all
+  end
+  
+  def show
   end
   
   def new
     @portfolio_item = Portfolio.new
   end
+ 
   def create
    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -17,4 +22,25 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  # def set_portfolio
+  #   @portfolio_item = Portfolio.find(params[:id])
+  # end
+  
 end
