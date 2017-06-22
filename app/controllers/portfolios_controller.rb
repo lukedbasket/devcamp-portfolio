872cfb975c8f08195319b069739e2bc8f78ff031
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 16:42:49 +0000

    Implemented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,19 @@
 class PortfoliosController < ApplicationController
-  # before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
-   @angular_portfolio_items = Portfolio.angular
+    @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
- 
+
   def create
-   @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -23,13 +23,14 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -38,21 +39,22 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
 end
